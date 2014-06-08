1f98b07ab2d8dfef70d0c6f0376e243e0a306523
techsathish <ittechsathish@gmail.com>
2014-06-09 01:37:43 +0530

    First Commit
    First Time Commit

---
+++ Site/MVC/App.js
@@ -0,0 +1,13 @@
+﻿var mySample = angular.module('mySample', []);
+
+mySample.directive('myDirective', function () {
+    return {
+        restrict: 'A',
+        replace: true,
+        scope: {
+            myUrl: '=someAttr',
+            myText: '@myLinkText'
+        },
+        template: '<div><input ng-model="myUrl" type="text" /><a href="{{myUrl}}">{{myText}}</a></div>'
+    };
+});
